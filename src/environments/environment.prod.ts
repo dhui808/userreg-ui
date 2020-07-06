65d82daa6935334ba8cfa4ba1e778d1df96fdf51
dannyhui <huichunwuca@yahoo.com>
2020-07-06 14:19:04 -0400

    environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
+  //apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://userreg1-dannyproject.apps.us-east-2.starter.openshift-online.com'
 };
